18b24120506185362d2629a35d40ebc33bd317cd
Martynas Gumuliauskas <Mourcius@gmail.com>
2023-03-02 21:25:55 +0200

    Updated Readme, added dependacies, Created router

---
+++ src/components/Layout/index.jsx
@@ -0,0 +1,11 @@
+import './index.scss'
+import React from 'react'
+
+const Layout = () => {
+  return (
+    <>
+      <p>Hello</p>
+    </>
+  )
+}
+export default Layout
---
+++ src/components/Sidebar/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './index.scss'
+
+const Sidebar = () => {
+    return (
+        <div>
+            <span>Helo from sidebar</span>
+        </div>
+    );
+};
+
+export default Sidebar
