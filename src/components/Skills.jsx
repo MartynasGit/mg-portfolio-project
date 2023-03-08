f422815805ab9d3077ecd45e1803e25ac6d10c6f
Martynas Gumuliauskas <Mourcius@gmail.com>
2023-03-08 21:26:37 +0200

    Added work Section.

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -12,7 +12,7 @@ const Skills = () => {
     <div name="skills" className="w-full h-screen bg-black text-gray-300">
       {/* Container */}
       <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center w-full h-full">
-        <div>
+        <div className="sm:text-center">
           <p className="text-4xl font-bold inline border-b-4 border-[#ed2835]">
             Skills
           </p>
