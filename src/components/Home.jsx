71cf793f4584b4845024889f189ef37e8c428a29
Martynas Gumuliauskas <Mourcius@gmail.com>
2023-03-28 20:00:02 +0300

    View work button funtionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { HiArrowNarrowRight } from 'react-icons/hi'
+import { Link } from 'react-scroll'
 
 const Home = () => {
   return (
@@ -14,11 +15,19 @@ const Home = () => {
           I'm a Full Stack developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
-          I'm junior full-stack developer, who always looking for self improvement.
+          I'm junior full-stack developer, who always looking for self
+          improvement.
         </p>
         <div>
           <button className="group text-white border-2 px-6 py-3 my-2 flex items-center hover:bg-[#ed2835] hover:border-[#ed2835]">
+            <Link
+              to="work"
+              smooth={true}
+              offset={50}
+              duration={500}
+            >
             View Work
+            </Link>
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-2" />
             </span>
