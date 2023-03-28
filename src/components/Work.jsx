f5ab0c2bf1b3465c0c2296ee15366d028a20e099
Martynas Gumuliauskas <Mourcius@gmail.com>
2023-03-28 18:14:57 +0300

    Updated Work section.

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -1,4 +1,7 @@
 import React from 'react'
+import Admin from '../assets/projects/admin.png'
+import Kvadratai from '../assets/projects/Kvadratai.png'
+import Todo from '../assets/projects/Todo.png'
 
 const Work = () => {
   return (
@@ -8,20 +11,94 @@ const Work = () => {
           <p className="text-4xl font-bold inline border-b-4 border-[#ed2835]">
             Work
           </p>
-          <p className='py-4'>// Check out some of my created projects</p>
+          <p className="py-4">// Check out some of my created projects</p>
         </div>
-        <div className='grid sm:grid-cols-2 md:grid-cols-3 gap-4'>
-          <div className='shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div'>
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 gap-4">
+          {/* Project Item */}
+          <div
+            style={{ backgroundImage: `url(${Admin})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
             {/* Hover Effects */}
-            <div>
+            <div className="opacity-0 group-hover:opacity-100">
               <div>
-                <span></span>
-                <div>
-                  <a href="/">
-                    <button></button>
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  PHP CMS application
+                </span>
+                <div className="pt-8 text-center">
+                  <a href="/" className="hidden">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
                   </a>
-                  <a href="/">
-                    <button></button>
+                  <a href="https://github.com/MartynasGit/cms" target="_blank">
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Project Item */}
+          <div
+            style={{ backgroundImage: `url(${Kvadratai})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div>
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  Square Game
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://martynasgit.github.io/kvadratai_game/"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/MartynasGit/kvadratai_game"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Project Item */}
+          <div
+            style={{ backgroundImage: `url(${Todo})` }}
+            className="shadow-lg shadow-[#040c16] group container rounded-md flex justify-center items-center mx-auto content-div"
+          >
+            {/* Hover Effects */}
+            <div className="opacity-0 group-hover:opacity-100">
+              <div>
+                <span className="text-2xl font-bold text-white tracking-wider">
+                  To-Do-List
+                </span>
+                <div className="pt-8 text-center">
+                  <a
+                    href="https://martynasgit.github.io/to-do-app/"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Demo
+                    </button>
+                  </a>
+                  <a
+                    href="https://github.com/MartynasGit/to-do-app"
+                    target="_blank"
+                  >
+                    <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                      Code
+                    </button>
                   </a>
                 </div>
               </div>
